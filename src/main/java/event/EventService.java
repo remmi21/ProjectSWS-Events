60b1ee1c6603a2e704dd9c52b4eb405066c1b71b
remmi21 <rimmi23@hotmail.com>
2018-04-26 12:12:20 +0200

    find Id

--- src/main/java/event/EventService.java
+++ src/main/java/event/EventService.java
@@ -1,4 +1,14 @@
 package event;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class EventService {
+    public static Map events = new HashMap();
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    public Event findById(String id) {
+        return (Event) events.get(id);
+    }
 }
