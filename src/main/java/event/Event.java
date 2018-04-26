0197689a80068a6c5985a2e04373a983f4ef63bf
remmi21 <rimmi23@hotmail.com>
2018-04-26 11:50:22 +0200

    Added Structure

--- src/main/java/event/Event.java
+++ src/main/java/event/Event.java
@@ -1,4 +1,17 @@
 package event;
 
+import java.util.Date;
+import java.util.List;
+
 public class Event {
+    public Integer id;
+    public String name;
+    public String describtion;
+    public String status;
+    public Integer limit;
+    public Integer tickets_left;
+    public Venue venue;
+    public Date date;
+    public List<Category> categories;
+
 }
