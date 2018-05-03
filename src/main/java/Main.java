91f48d44275015882d42312efe953a8d258c80c5
remmi21 <rimmi23@hotmail.com>
2018-05-03 11:31:15 +0200

    working state

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,22 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import event.EventService;
-
+import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class Main {
-    private static EventService userService = new EventService();
+    private static EventService eventService = new EventService();
     private static ObjectMapper om = new ObjectMapper();
 
 
     public static void main(String [] args) {
         EventService e= new EventService();
-        e.loadMongoEvents();
+       // e.loadMongoEvents();
 
         // Start embedded server at this port
         port(8080);
 
         // Main Page, welcome
-        //get("/", (request, response) -> "Welcome"); // this is not working ?
+        get("/", (request, response) -> "Welcome"); // this is not working ?
 
 
     }
