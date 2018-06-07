f12edffd0e960eca0b6834f6ee0dc4e665273c4c
remmi21 <rimmi23@hotmail.com>
2018-06-07 14:32:36 +0200

    location annotated

--- src/main/java/event/CostumLocationSerializer.java
+++ src/main/java/event/CostumLocationSerializer.java
@@ -22,14 +22,21 @@ public class CostumLocationSerializer extends StdSerializer<Location> {
     @Override
     public void serialize(Location location, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeStringField("@type", "\"@type\":\"http://schema.org/PostalAddress\"");
+        jsonGenerator.writeObjectFieldStart("@context");
+            jsonGenerator.writeStringField("address", "http://schema.org/streetAddress ");
+            jsonGenerator.writeStringField("city", "http://schema.org/addressLocality");
+            jsonGenerator.writeStringField("state", "http://schema.org/addressRegion");
+            jsonGenerator.writeStringField("country", "http://schema.org/addressRegion");
+            jsonGenerator.writeStringField("zip", "http://schema.org/postalCode");
+        jsonGenerator.writeEndObject();
+        jsonGenerator.writeStringField("@type", "http://schema.org/PostalAddress");
         jsonGenerator.writeStringField("address", location.getAddress());
         jsonGenerator.writeStringField("city", location.getCity());
         jsonGenerator.writeStringField("zip", location.getZipcode());
         jsonGenerator.writeStringField("country", location.getCountry());
         jsonGenerator.writeStringField("state", location.getState());
         jsonGenerator.writeEndObject();
-        
+
         String serialized = mapper.writeValueAsString(JsonldResource.Builder.create().build(location));
     }
 }
